20a2f56f0bfadeebd3f95405bbd59d2f5b7727be
Richard Franks <git@rf152.co.uk>
2020-07-13 16:32:56 +0100

    Removed unused file

--- files/startup.sh
+++ files/startup.sh
@@ -3,8 +3,5 @@
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
     echo "SSH - not starting camera"
 else
-    touch /run/user/1000/neopixel.state
-
-    sudo /opt/raspindi/bin/neopixel &
     /opt/raspindi/bin/camera
 fi
